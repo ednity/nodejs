9a762249b796157cc98f83a22e0abeb0550b94de
yu1ch1 <s.yuichi227@gmail.com>
2014-10-28 23:59:09 +0900

    InitialCommit

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['staging-socket'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'ad2c19b97a0d82c3e1a2cd6dee7539498e5d8b41',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
---
+++ timeline_node.js
@@ -0,0 +1,58 @@
+require('newrelic');
+var http = require("http");
+var url = require('url');
+var socketio = require("socket.io");
+var fs = require("fs");
+
+var server = http.createServer().listen(process.env.VMC_APP_PORT || 3001);
+var io = socketio.listen(server);
+
+// notificationのping用
+server.on('request', function(request, response) {
+	var urlElements = url.parse(request.url, true);
+	var query = urlElements.query;
+
+	if (query.type == 'ping') {
+		io.sockets.in(query.group_id).emit("notification", { type: "ping" });
+	}
+});
+
+
+io.sockets.on("connection", function (socket) {
+
+	/* Join room */
+	socket.on('initialize', function(data) {
+		socket.join(data.group_id);
+	});
+
+	/* Broadcast the data */
+	socket.on("post", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("post", data)
+		.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+	socket.on("comment", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("comment", data)
+		.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+	socket.on("like", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("like", data)
+		.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+	socket.on("qa_answer", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("qa_answer", data)
+		.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+	socket.on("notification", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+	socket.on("poll_check", function (data) {
+		socket
+		.broadcast.to(data.group_id).emit("poll_check", data);
+		//.broadcast.to(data.group_id).emit("notification", { type: "ping" });
+	});
+});
